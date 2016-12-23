868f9298689c2161c42cf7c369ee81e72d440dbc
jhon jairo <jjms95@hotmail.com>
2016-12-22 22:12:12 -0500

    working on the sign_in with facebook,gplus and twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
+	TEMP_EMAIL_PREFIX = 'change@me'
+  	TEMP_EMAIL_REGEX = /\Achange@me/
 	rolify
 	devise :database_authenticatable, :registerable,
-			:recoverable, :rememberable, :trackable, :validatable
+			:recoverable, :rememberable, :trackable, :validatable, :omniauthable
 	after_create :assign_default_role
 	#donor side of the relationship
 	has_many :maked_donations, :class_name => 'Donation', :foreign_key => 'donor_id'
-  	#student side of the ralationship
-  	has_many :recieved_donations, :class_name => 'Donation', :foreign_key => 'student_id'
-  	#child side of the relationship
-  	has_one :child_parent_child, :class_name => 'ParentChild', :foreign_key => 'child_id'
-  	has_one :parent, :class_name => 'User', :foreign_key => 'id',through: :child_parent_child
-  	#parent side of the relationship
-  	has_many :parent_parent_child, :class_name => 'ParentChild', :foreign_key => 'parent_id'
-  	has_many :childs, :class_name => 'User', :foreign_key => 'id',through: :parent_parent_child
+		#student side of the ralationship
+		has_many :recieved_donations, :class_name => 'Donation', :foreign_key => 'student_id'
+		#child side of the relationship
+		has_one :child_parent_child, :class_name => 'ParentChild', :foreign_key => 'child_id'
+		has_one :parent, :class_name => 'User', :foreign_key => 'id',through: :child_parent_child
+		#parent side of the relationship
+		has_many :parent_parent_child, :class_name => 'ParentChild', :foreign_key => 'parent_id'
+		has_many :childs, :class_name => 'User', :foreign_key => 'id',through: :parent_parent_child
 	
 	#methods
 	def assign_default_role
@@ -38,6 +40,55 @@ class User < ApplicationRecord
 	end	
 	def normal?
 		self.has_role? :normal
+	end
+
+	#multiple account
+	validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
+
+	def self.find_for_oauth(auth, signed_in_resource = nil)
+
+		# Get the identity and user if they exist
+		identity = Identity.find_for_oauth(auth)
+
+		# If a signed_in_resource is provided it always overrides the existing user
+		# to prevent the identity being locked with accidentally created accounts.
+		# Note that this may leave zombie accounts (with no associated identity) which
+		# can be cleaned up at a later date.
+		user = signed_in_resource ? signed_in_resource : identity.user
+
+		# Create the user if needed
+		if user.nil?
+
+			# Get the existing user by email if the provider gives us a verified email.
+			# If no verified email was provided we assign a temporary email and ask the
+			# user to verify it on the next step via UsersController.finish_signup
+			email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+			email = auth.info.email if email_is_verified
+			user = User.where(:email => email).first if email
+
+			# Create the user if it's a new registration
+			if user.nil?
+				user = User.new(
+					name: auth.extra.raw_info.name,
+					#username: auth.info.nickname || auth.uid,
+					email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+					password: Devise.friendly_token[0,20]
+				)
+				user.skip_confirmation!
+				user.save!
+			end
+		end
+
+		# Associate the identity with the user if needed
+		if identity.user != user
+			identity.user = user
+			identity.save!
+		end
+		user
+	end
+
+	def email_verified?
+		self.email && self.email !~ TEMP_EMAIL_REGEX
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
     	root :to => "main#dashboard"
     	# Rails 4 users must specify the 'as' option to give it a unique name
     	# root :to => "main#dashboard", :as => "authenticated_root"
-  end
+  	end
 	root to: 'static_pages#home', 
 	constraints: lambda { |request| !request.env['warden'].user}
 
 	#devise
-	devise_for :users, controllers: { registrations: "users/registrations",sessions:"users/sessions" }
+	devise_for :users, controllers: { registrations: "users/registrations",sessions:"users/sessions" ,omniauth_callbacks: 'users/omniauth_callbacks'}
 	as :user do
 		resources :donations
 		resources :parent_children
@@ -22,4 +22,7 @@ Rails.application.routes.draw do
 	get '/assign_roles/first_sign', to: 'assign_roles#first_sign', as: 'first_sign'
 	get '/assign_roles/save_role', to: 'assign_roles#save_role', as: 'save_role'
 	get '/static_pages/home', to: 'static_pages#home', as: 'home'
+
+	#multiple account
+	match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 end
